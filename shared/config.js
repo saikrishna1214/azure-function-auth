75aac1fbaa7a29719a1713ecb7bce01e04134d12
saikrishna1214 <35663289+saikrishna1214@users.noreply.github.com>
2025-08-06 17:03:43 +0530

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  clientId: 'YOUR_CLIENT_ID',
-  clientSecret: 'YOUR_CLIENT_SECRET',
-  tenantId: 'YOUR_TENANT_ID',
+  clientId: '41487f12-4cbf-42e2-962d-85d3e29e4cf3',
+  clientSecret: 'HdB8Q~q3bMG_S9XIbMv10iTX1rxV~m6qk1QDFcU2',
+  tenantId: '0012543a-073f-4274-98aa-41cf63d531ce',
   redirectUri: 'https://<your-fn-app>.azurewebsites.net/api/auth/callback',
   scopes: ['User.Read', 'Chat.Read']
 };
